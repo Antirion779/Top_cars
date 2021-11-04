5b1834efe5eac18d1535992c5a8d2527b94d8fee
Antirion <73186199+Antirion779@users.noreply.github.com>
2021-11-04 15:51:25 +0100

    Car Mouvement Fonctionnal

--- Assets/Script/CarsMouvement.cs
+++ Assets/Script/CarsMouvement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CarsMouvement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Component")] 
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private GameObject front;
+
+    [Header("constant")] 
+    [SerializeField] private float speed = 2.0f;
+
+    [SerializeField] private float maxSpeed = 20.0f;
+    [SerializeField] private float speedRotation = 2.0f;
+    private Vector2 dir;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        dir = front.transform.position - transform.position;
+        Debug.Log(rb.velocity.magnitude);
+
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            if (Input.GetKey(KeyCode.Keypad8))//Up
+            {
+                rb.AddForce(dir * speed);
+                Debug.Log("Forward");
+            }
+            if (Input.GetKey(KeyCode.Keypad2))//Down
+            {
+                rb.AddForce(Vector2.up * -speed);
+                Debug.Log("backward");
+            }
+        }
         
+
+        if (Input.GetKey(KeyCode.Keypad4))//Left
+        {
+            transform.Rotate(new Vector3(0,0,1) * speedRotation * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Keypad6))//Right
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * -speedRotation * Time.deltaTime);
+        }
+
     }
 }
