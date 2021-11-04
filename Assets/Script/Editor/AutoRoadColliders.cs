ddaa35cba49b32be8e709bd00ded26cbb767413d
Outfoxeed <letourneaumorigan@gmail.com>
2021-11-04 23:40:36 +0100

    First created track prefab

--- Assets/Script/Editor/AutoRoadColliders.cs
+++ Assets/Script/Editor/AutoRoadColliders.cs
@@ -16,68 +16,94 @@ public class AutoRoadColliders : EditorWindow
     public GameObject colliderTarget;
 
     float distance = 0.2f;
+    bool debugDistance;
 
     private void OnGUI()
     {
-        // Line renderer
-        if (GUILayout.Button("Fix line renderer Z points"))
-        {
-            for (int i = 0; i < lr.positionCount; i++)
-            {
-                Vector3 newPos = lr.GetPosition(i);
-                newPos.z = 0;
-                lr.SetPosition(i, newPos);
-            }
-        }
-        using (new EditorGUILayout.HorizontalScope())
-        {
-            GUILayout.Label("Line renderer");
-            lr = (LineRenderer)EditorGUILayout.ObjectField(lr, typeof(LineRenderer), true);
-        }
+        GUILayout.Label("First params");
 
-        // Collider target
-        using (new EditorGUILayout.HorizontalScope())
+        // Line renderer
+        using (new EditorGUILayout.VerticalScope())
         {
-            GUILayout.Label("Collider target");
-            colliderTarget = (GameObject)EditorGUILayout.ObjectField(colliderTarget, typeof(GameObject), true);
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Label("Line renderer");
+                lr = (LineRenderer)EditorGUILayout.ObjectField(lr, typeof(LineRenderer), true);
+                if (GUILayout.Button("Fix line renderer Z points"))
+                {
+                    Undo.RecordObject(lr, "Set all points Z coordinates to 0");
+                    for (int i = 0; i < lr.positionCount; i++)
+                    {
+                        Vector3 newPos = lr.GetPosition(i);
+                        newPos.z = 0;
+                        lr.SetPosition(i, newPos);
+                    }
+                }
+            }
+
+            // Collider target
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Label("Collider target");
+                colliderTarget = (GameObject)EditorGUILayout.ObjectField(colliderTarget, typeof(GameObject), true);
+                if (GUILayout.Button("Remove"))
+                {
+                    if(colliderTarget != null) colliderTarget = null;
+                }
+            }
         }
 
         // Functions
         GUILayout.Space(10);
 
-            // Fill function
+        // Fill function
         if (GUILayout.Button("Fill")) AdaptColliderToFill();
         GUILayout.Space(10);
 
+        // Other functions
+        using (new GUILayout.HorizontalScope())
+        {
+            GUILayout.Label("Line thickness");
+            distance = EditorGUILayout.FloatField(distance);
+            //debugDistance = EditorGUILayout.Toggle(debugDistance);
+        }
+        using (new GUILayout.VerticalScope())
+        {
             // Around function
-        using (new GUILayout.HorizontalScope())
-        {
-            distance = EditorGUILayout.FloatField(distance);
             if (GUILayout.Button("Around")) AdaptColliderAround(distance);
-        }
-        GUILayout.Space(5);
-
+            GUILayout.Space(5);
             // Interior border only function
-        using (new GUILayout.HorizontalScope())
-        {
-            distance = EditorGUILayout.FloatField(distance);
             if (GUILayout.Button("Inside border")) AdaptColliderAround(distance, true, false);
-        }
-        GUILayout.Space(5);
-
+            GUILayout.Space(5);
             // Exterior border only function
-        using (new GUILayout.HorizontalScope())
-        {
-            distance = EditorGUILayout.FloatField(distance);
             if (GUILayout.Button("Exterior border")) AdaptColliderAround(distance, false, true);
         }
+
+
+        //if (debugDistance)
+        //{
+        //    if(lr != null)
+        //    {
+        //        Gizmos.color = Color.cyan;
+        //        for (int i = 0; i < lr.positionCount; i++)
+        //        {
+        //            Handles.CircleHandleCap(500, lr.GetPosition(i), Quaternion.identity, distance, EventType.Ignore);
+        //            //Gizmos.DrawWireSphere(lr.GetPosition(i), distance);
+        //        }
+        //    }
+        //}
     }
 
     public void AdaptColliderToFill()
     {
         if (lr == null) return;
-        if (colliderTarget == null) colliderTarget = new GameObject("Collider target");
-        EdgeCollider2D edgeCollider = colliderTarget.AddComponent<EdgeCollider2D>();
+        if (colliderTarget == null)
+        {
+            colliderTarget = new GameObject("Collider target");
+            Selection.SetActiveObjectWithContext(colliderTarget, colliderTarget);
+        }
+        EdgeCollider2D edgeCollider = FindEdgeCollider();
 
         List<Vector2> newPoints = new List<Vector2>();
         for (int i = 0; i < lr.positionCount; i++)
@@ -88,8 +114,9 @@ public class AutoRoadColliders : EditorWindow
         {
             newPoints.Add(lr.GetPosition(i));
         }
-        edgeCollider.SetPoints(newPoints);
 
+        Undo.RecordObject(edgeCollider, "Changed edga radius and all points of EdgeCollider2D");
+        edgeCollider.SetPoints(newPoints);
         edgeCollider.edgeRadius = lr.startWidth / 2f;
     }
     public void AdaptColliderAround(float distance, bool interior = true, bool exterior = true)
@@ -97,7 +124,8 @@ public class AutoRoadColliders : EditorWindow
         // Initialize
         if (lr == null) return;
         if (colliderTarget == null) colliderTarget = new GameObject("Collider target");
-        EdgeCollider2D edgeCollider = colliderTarget.AddComponent<EdgeCollider2D>();
+
+        EdgeCollider2D edgeCollider = FindEdgeCollider();
 
         List<Vector2> newPoints = new List<Vector2>();
 
@@ -131,7 +159,15 @@ public class AutoRoadColliders : EditorWindow
         }
 
         // Set points and edge radius
+        Undo.RecordObject(edgeCollider, "Changed edga radius and all points of EdgeCollider2D");
         edgeCollider.SetPoints(newPoints);
         edgeCollider.edgeRadius = lr.startWidth / 10f;
+    }
+
+    EdgeCollider2D FindEdgeCollider()
+    {
+        EdgeCollider2D result;
+        if (colliderTarget.TryGetComponent<EdgeCollider2D>(out result)) return result;
+        else return colliderTarget.AddComponent<EdgeCollider2D>();
     }
 }
